4d49c5cc21412d776d87b9d94fe2fab628b4f2fd
SourSoulSoup <SourSoulSoup@gmail.com>
2014-12-08 11:11:40 -0600

    Routine Push

---
+++ UR Investing/UR Investing/Models/MemberModels.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UR_Investing.Models
+{
+    public class Member
+    {
+        [Key]
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [DataType(DataType.ImageUrl)]
+        [FileExtensions(ErrorMessage = "Please enter a valid file name with the extension \".png, .jpg, .jpeg, or .gif\"", Extensions = ".png,.jpg,.jpeg,.gif")]
+        public string picturePath { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string biography { get; set; }
+
+        public string resumePath { get; set; }
+    }
+}

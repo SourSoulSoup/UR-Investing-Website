a19b115e577f1e012b02557af6f3b8fc39a4dfd2
SourSoulSoup <SourSoulSoup@gmail.com>
2015-03-09 13:07:29 -0600

    Formatting and Style Update
    Colour scheme established.
    Front page stylized better.

---
+++ UR Investing/UR Investing/Controllers/LayoutController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UR_Investing.Controllers
+{
+    public class LayoutController : Controller
+    {
+        public ActionResult MemberList()
+        {
+            return PartialView("~/Views/Shared/_MemberList.cshtml");
+        }
+    }
+}
